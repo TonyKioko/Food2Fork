ec0263f2c9e2be619b8be5aec69562837448960d
TonyKioko <tonykioko384@gmail.com>
2019-02-27 14:21:39 +0300

    using temporary data to avoid api throttling

--- src/App.js
+++ src/App.js
@@ -10,25 +10,25 @@ import RecipeDetails from './components/RecipeDetail';
 class App extends Component {
 
   state = {
-    recipes:[],
+    recipes:[recipes],
     url:'https://www.food2fork.com/api/search?key=706e34a6cd0040671f82652ba3e62c88'
   }
 
-  async getRecipes(){
-    try{
-      const data = await fetch(this.state.url)
-      const jsonData = await data.json()
-      this.setState({
-        recipes:jsonData.recipes
-      })
-    }catch(error){
-console.log(error)
-    }
-  }
+//   async getRecipes(){
+//     try{
+//       const data = await fetch(this.state.url)
+//       const jsonData = await data.json()
+//       this.setState({
+//         recipes:jsonData.recipes
+//       })
+//     }catch(error){
+// console.log(error)
+//     }
+//   }
 
-  componentDidMount() {
-    this.getRecipes()
-  }
+//   componentDidMount() {
+//     this.getRecipes()
+//   }
   render() {
 console.log(this.state.recipes)
 
